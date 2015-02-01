19aecdf3bf3b1854c1ecc8fe197afa7ac2a185b1
yukkat <iagisha@gmail.com>
2015-02-01 15:56:19 +0400

    figure class

--- js/main.js
+++ js/main.js
@@ -1,69 +1,90 @@
-/* options */
+// Options
 var cageSize = 30;
 
-var cageSizepx = cageSize + 'px';
 var box = document.getElementById('box');
 var area = document.getElementById('area');
+var boxWidth = box.offsetWidth;
+var boxHeight = box.offsetHeight;
 
-/* calculates */
-var areaCalculates = function () {
-	var boxWidth = box.offsetWidth;
-	var boxHeight = box.offsetHeight;
-	var cagesInWidth = parseInt(boxWidth / cageSize);
-	var cagesInHeight = parseInt(boxHeight / cageSize);
-	area.style.width = cagesInWidth * cageSize + 'px';
-	var areaHeight = cagesInHeight*cageSize;
-	area.style.paddingTop = (boxHeight - areaHeight)/2 + 'px';
+var cagesInWidth = parseInt(boxWidth / cageSize);
+if (cagesInWidth % 2 === 0) {
+	cagesInWidth = cagesInWidth - 1;
+}
+var cageSizepx = cageSize + 'px';
+var cagesInHeight = parseInt(boxHeight / cageSize);
+area.style.width = cagesInWidth * cageSize + 'px';
+var areaHeight = cagesInHeight * cageSize;
+area.style.paddingTop = (boxHeight - areaHeight) / 2 + 'px';
 
-	var numberOfCages = cagesInWidth * cagesInHeight;
+var numberOfCages = cagesInWidth * cagesInHeight;
 
-	console.log(boxWidth + ',' + boxHeight + ',' + numberOfCages);
+console.log(boxWidth + ',' + boxHeight + ',' + numberOfCages);
 
-	for ( var i = 0; i < numberOfCages; i++ ) {
-		area.appendChild(document.createElement("div"));
+// Grid creating
+for (var i = 0; i < numberOfCages; i++) {
+	area.appendChild(document.createElement("div"));
+}
+
+var cages = area.querySelectorAll('div');
+
+var xaxis = 1, yaxis = 1;
+
+for (var item in cages) {
+	var cage = cages.item(item);
+	cage.style.width = cageSizepx;
+	cage.style.height = cageSizepx;
+	cage.setAttribute('id', 'x' + xaxis + 'y' + yaxis);
+	xaxis = xaxis + 1;
+	if (xaxis === cagesInWidth + 1) {
+		yaxis = yaxis + 1;
+		xaxis = 1;
 	}
+}
 
-	var cages = area.querySelectorAll('div');
 
-	for ( var item in cages ) {
-		var cage = cages.item(item);
-		cage.style.width = cageSizepx;
-		cage.style.height = cageSizepx;
-		cage.setAttribute('id', 'x'+xaxis + 'y' + yaxis);
-		xaxis = xaxis + 1;
-		if (xaxis === cagesInWidth + 1) {
-			yaxis = yaxis + 1;
-			xaxis = 1;
+// creating figure class
+function Figure() {
+	this.xpos = parseInt(cagesInWidth / 2);
+	this.ypos = 1;
+	this.initializeFigure = function (index) {
+		var cubes = document.querySelectorAll('.cube'+index);
+		var cubesLength = cubes.length;
+		for (i = 0; i < cubesLength; i++) {
+			cubes.item(i).removeAttribute('class');
+		}
+		document.getElementById('x' + this.xpos + 'y' + this.ypos).className = 'cube'+index;
+		document.getElementById('x' + this.xpos + 'y' + (this.ypos + 1)).className = 'cube'+index;
+	};
+	this.fastMoveDown = function (index) {
+		this.initializeFigure(index);
+		if (this.ypos != cagesInHeight - 1) {
+			this.ypos = this.ypos + 1;
+			this.fastMoveDown();
+		}
+	};
+	this.stepDown = function (index) {
+		this.initializeFigure(index);
+		if (this.ypos != cagesInHeight - 1) {
+			this.ypos = this.ypos + 1;
+		}
+	};
+	this.stepLeft = function (index) {
+		this.initializeFigure(index);
+		if (this.xpos !== 1) {
+			this.xpos = this.xpos - 1;
+		}
+	};
+	this.stepRight = function (index) {
+		this.initializeFigure(index);
+		if (this.xpos !== cagesInWidth) {
+			this.xpos = this.ypos - 1;
 		}
 	}
+}
 
+// Creating figures
+var cube1 = new Figure();
+var cube2 = new Figure();
 
-	// creating figure class
-	function Figure() {
-		//this.index = index;
-		this.xpos = parseInt(cagesInWidth / 2);
-		this.ypos = 1;
-		this.initializeFigure = function () {
-			var cubes = document.querySelectorAll('.cube');
-			var cubesLength = cubes.length;
-			for ( i = 0; i < cubesLength; i++ ) {
-				cubes.item(i).removeAttribute('class');
-			}
-			document.getElementById('x'+this.xpos+'y'+this.ypos).className = 'cube';
-			document.getElementById('x'+this.xpos+'y'+(this.ypos+1)).className = 'cube';
-		};
-		this.moveDown = function(){
-			this.initializeFigure()
-			if(this.ypos!=cagesInHeight-1){
-				this.ypos = this.ypos + 1;
-				this.moveDown();
-			}
-		}
-	}
-
-	var cube1 = new Figure();
-
-	cube1.moveDown();
-
-}();
-
+cube1.initializeFigure(1);
+cube2.initializeFigure(2);
