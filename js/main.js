84384fcf336dbb4fb445abcb46a1cac844f88e81
yukkat <iagisha@gmail.com>
2015-02-14 16:59:21 +0400

    конец игры, уничтожение строки

--- js/main.js
+++ js/main.js
@@ -1,8 +1,11 @@
 // Options
-var cageSize = 30;
+var cageSize = 45;
 
-var box = document.getElementById('box');
+// nodes
+var box = document.body;
 var area = document.getElementById('area');
+
+// area calculates
 var boxWidth = box.offsetWidth;
 var boxHeight = box.offsetHeight;
 
@@ -10,7 +13,7 @@ var cagesInWidth = parseInt(boxWidth / cageSize);
 if (cagesInWidth % 2 === 0) {
 	cagesInWidth = cagesInWidth - 1; //always odd
 }
-var cageSizepx = cageSize + 'px';
+
 var cagesInHeight = parseInt(boxHeight / cageSize);
 area.style.width = cagesInWidth * cageSize + 'px';
 var areaHeight = cagesInHeight * cageSize;
@@ -22,7 +25,7 @@ var numberOfCages = cagesInWidth * cagesInHeight;
 console.log(boxWidth + ',' + boxHeight + ',' + numberOfCages);
 
 // Grid creating
-for ( var i = 0; i < numberOfCages; i++ ) {
+for (var i = 0; i < numberOfCages; i++) {
 	area.appendChild(document.createElement("div"));
 }
 
@@ -31,11 +34,12 @@ var cagesLength = cages.length;
 
 var xaxis = 1, yaxis = 1;
 
-for ( i = 0; i < cagesLength; i++ ) {
+for (i = 0; i < cagesLength; i++) {
 	var cage = cages.item(i);
-	cage.style.width = cageSizepx;
-	cage.style.height = cageSizepx;
+	cage.style.width = cageSize + 'px';
+	cage.style.height = cageSize + 'px';
 	cage.setAttribute('id', 'x' + xaxis + 'y' + yaxis);
+	cage.setAttribute('y', yaxis);
 	xaxis = xaxis + 1;
 	if (xaxis === cagesInWidth + 1) {
 		yaxis = yaxis + 1;
@@ -44,24 +48,28 @@ for ( i = 0; i < cagesLength; i++ ) {
 }
 
 var forget = false;
+var fastMoveInterval;
 
 // creating figure class
-function Figure(index) {
+function Figure() {
 	this.xpos = parseInt(cagesInWidth / 2) + 1; // always centered
 	this.ypos = 1;
 	this.initializeFigure = function () {
-		var cubes = document.querySelectorAll('.cube.active');
+		var cubes = document.querySelectorAll('[active]');
 		var cubesLength = cubes.length;
-		for ( i = 0; i < cubesLength; i++ ) {
+		for (i = 0; i < cubesLength; i++) {
 			cubes.item(i).removeAttribute('class');
+			cubes.item(i).removeAttribute('active');
 		}
-		document.getElementById('x' + this.xpos + 'y' + this.ypos).className = 'cube active';
+		document.getElementById('x' + this.xpos + 'y' + this.ypos).className = 'cube';
+		document.getElementById('x' + this.xpos + 'y' + this.ypos).setAttribute('active', '');
 	};
 	this.stepLeft = function () {
 
 		if (this.xpos !== 1) {
 			--this.xpos;
-		}this.initializeFigure();
+		}
+		this.initializeFigure();
 	};
 	this.stepRight = function () {
 		if (this.xpos !== cagesInWidth) {
@@ -73,39 +81,41 @@ function Figure(index) {
 		this.initializeFigure();
 		var nextCage = document.getElementById('x' + this.xpos + 'y' + (
 		this.ypos + 1));
-		if (this.ypos !== cagesInHeight - 1 || nextCage&&nextCage.className !== "cube") {
+
+		if (nextCage && nextCage.className !== "cube") {
 			++this.ypos;
+		} else if (this.ypos === 1) {
+			var reload = confirm('Игра закончена! Хотите перезапустить?');
+			if (reload) {
+				location.reload()
+			} else {
+				clearInterval(movingInterval, newObjectInterval)
+			}
 		} else {
-			var cubes = document.querySelectorAll('.cube.active');
+			var cubes = document.querySelectorAll('[active]');
 			var cubesLength = cubes.length;
-			for ( i = 0; i < cubesLength; i++ ) {
-				cubes.item(i).removeAttribute('class');
+			for (i = 0; i < cubesLength; i++) {
+				cubes.item(i).removeAttribute('active');
 			}
 			document.getElementById('x' + this.xpos + 'y' + this.ypos).className = 'cube';
+
 			return forget = true;
 		}
 
 	};
 	this.fastMoveDown = function () {
+		clearInterval(fastMoveInterval);
+		fastMoveInterval = setInterval(function () {
+			cube.stepDown();
+		}, 50);
 
-		this.initializeFigure();
-
-		if (this.ypos != cagesInHeight) {
-			var nextCage = document.getElementById('x' + this.xpos + 'y' + (
-			this.ypos + 1));
-			if (nextCage && nextCage.className !== "cube") {
-				++this.ypos;
-				this.fastMoveDown();
-
-			}
-		}return forget = true;
 	};
 }
 
 //keyboard handling
 window.addEventListener('keydown', function (e) {
 	if (e.keyCode === 32) {//space
-		cube.stepDown();
+		cube.fastMoveDown();
 	} else if (e.keyCode === 37) {//left
 		cube.stepLeft();
 	} else if (e.keyCode === 39) {//right
@@ -115,18 +125,35 @@ window.addEventListener('keydown', function (e) {
 	}
 });
 
-var index;
-// Creating figures
-var cube = new Figure(index);
-
+var cube = new Figure();
 cube.initializeFigure();
 
-setInterval(function(){
-	if (forget){
-		++index;
-		cube = new Figure(index);
-		cube.xpos = parseInt(cagesInWidth / 2) + 1; // always centered
-		cube.ypos = 1;
+var movingInterval = setInterval(function () {
+	cube.stepDown();
+}, 1000);
+
+var lastLine = document.querySelectorAll('[y="' + cagesInHeight + '"]');
+
+var newObjectInterval = setInterval(function () {
+	if (forget) {
+		clearInterval(fastMoveInterval);
+		cube = new Figure();
 		cube.initializeFigure();
+		var fullness = 0;
+		for (i = 0; i < lastLine.length; i++) {
+			if (lastLine.item(i).className === "cube") {
+				fullness++;
+			}
+		}
+		if (fullness === cagesInWidth) {
+			setTimeout(function () {
+				for (i = 0; i < lastLine.length; i++) {
+					lastLine.item(i).removeAttribute('class');
+				}
+			}, 500)
+		}
+
+		return forget = false;
 	}
-},100);
+}, 100);
+
